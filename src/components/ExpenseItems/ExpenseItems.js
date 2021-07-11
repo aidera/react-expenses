7dbfcd3bb5dcf9f37c62076414b380162e7b5965
Evgenia Mikhaleva <83216721+EugeniaM-DIGIS@users.noreply.github.com>
2021-07-11 17:34:41 +0300

    Add no items error

--- src/components/ExpenseItems/ExpenseItems.js
+++ src/components/ExpenseItems/ExpenseItems.js
@@ -1,13 +1,18 @@
 import Card from '../Card/Card';
 import ExpenseItem from './ExpenseItem/ExpenseItem';
+import './ExpenseItems.css';
 
 function ExpenseItems(props) {
   const { items, removeExpense } = props;
   return (
     <Card>
-      {items.map((el) => (
-        <ExpenseItem key={el.id} expense={el} removeExpense={removeExpense} />
-      ))}
+      {items.length <= 0 ? (
+        <span className="expense-items__error">There are no expenses for now</span>
+      ) : (
+        items.map((el) => (
+          <ExpenseItem key={el.id} expense={el} removeExpense={removeExpense} />
+        ))
+      )}
     </Card>
   );
 }
