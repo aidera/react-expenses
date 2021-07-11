ad11cf00a033f9671c996034d9a0a913892f151e
Evgenia Mikhaleva <83216721+EugeniaM-DIGIS@users.noreply.github.com>
2021-07-11 16:42:42 +0300

    Made correct add expense function

--- src/App.js
+++ src/App.js
@@ -4,18 +4,20 @@ import AddExpense from './components/AddExpense/AddExpense';
 import ExpenseItems from './components/ExpenseItems/ExpenseItems';
 import ExpensesPanel from './components/ExpensesPanel/ExpensesPanel';
 
+const DUMMY_EXPENSES = [
+  {
+    id: 1,
+    title: 'Car Insurance',
+    amount: 294.67,
+    date: new Date(2021, 2, 14),
+  },
+  { id: 2, title: 'Books', amount: 20.0, date: new Date(2021, 5, 22) },
+  { id: 3, title: 'Food', amount: 402.15, date: new Date(2021, 8, 5) },
+  { id: 4, title: 'Food2', amount: 402.15, date: new Date(2019, 2, 14) },
+];
+
 function App() {
-  const [expenses, setExpenses] = useState([
-    {
-      id: 1,
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 14),
-    },
-    { id: 2, title: 'Books', amount: 20.0, date: new Date(2021, 5, 22) },
-    { id: 3, title: 'Food', amount: 402.15, date: new Date(2021, 8, 5) },
-    { id: 4, title: 'Food2', amount: 402.15, date: new Date(2019, 2, 14) },
-  ]);
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const removeExpense = (id) => {
     const newExpenses = expenses.filter(el => el.id !== id);
@@ -23,7 +25,9 @@ function App() {
   };
 
   const addExpense = (expense) => {
-    setExpenses([expense, ...expenses]);
+    setExpenses((prevExpenses) => {
+      return [expense, ...prevExpenses]
+    });
   };
 
   return (
